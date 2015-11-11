f74718a3d8d6437f27c283dedfbb3f97ef5cd5d8
Johnpavy <johnpavelchak@yahoo.com>
2015-11-11 14:23:05 -0600

    First commit

---
+++ ProjectManagement/Login.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="ProjectManagement.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+     <div style="text-align: center">
+    
+        <asp:TextBox ID="EmailTxtBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="EmailLbl" runat="server" Text="Email"></asp:Label>
+        <br />
+        <asp:TextBox ID="PasswordTxtBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="PasswordLbl" runat="server" Text="Password"></asp:Label>
+        <br />
+        <asp:Button ID="LoginBtn" runat="server" Text="Login" OnClick="LoginBtn_Click" />
+&nbsp;&nbsp;
+        <asp:Button ID="SignUpBtn" runat="server" OnClick="SignUpBtn_Click" Text="Sign Up" />
+        <br />
+        <asp:TextBox ID="NewEmailTxtBox" runat="server" Visible="False"></asp:TextBox>
+        <br />
+        <asp:Label ID="SignupEmailLbl" runat="server" Text="Email" Visible="False"></asp:Label>
+        <br />
+        <asp:TextBox ID="FNameTxtBox" runat="server" Visible="False"></asp:TextBox>
+        <br />
+        <asp:Label ID="FNameLbl" runat="server" Text="First Name"></asp:Label>
+        <br />
+        <asp:TextBox ID="LNameTxtbox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="LastNameLbl" runat="server" Text="Last Name" Visible="False"></asp:Label>
+        <br />
+        <asp:TextBox ID="NewPassTxtBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="NewPasswordLbl" runat="server" Text="Password" Visible="False"></asp:Label>
+        <br />
+        <asp:TextBox ID="ConfirmPassTxtBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="ConfirmPWLbl" runat="server" Text="Confirm Password"></asp:Label>
+        <br />
+        <asp:Button ID="RegisterBtn" runat="server" Text="Register" OnClick="RegisterBtn_Click" />
+        <br />
+        <asp:Label ID="error1" runat="server" Text="Label" Visible="False"></asp:Label>
+        <br />
+        <asp:Label ID="error2" runat="server" Text="Label" Visible="False"></asp:Label>
+        <br />
+        <br />
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:inetConnectionString %>" SelectCommand="SELECT * FROM [attachment]"></asp:SqlDataSource>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ProjectManagement/Login.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace ProjectManagement
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            error2.Visible = false;
+            error1.Visible = false;
+            EmailLbl.Visible = true;
+            EmailTxtBox.Visible = true;
+            PasswordLbl.Visible = true;
+            PasswordTxtBox.Visible = true;
+            NewEmailTxtBox.Visible = false;
+            SignupEmailLbl.Visible = false;
+            NewPassTxtBox.Visible = false;
+            NewPasswordLbl.Visible = false;
+            FNameLbl.Visible = false;
+            FNameTxtBox.Visible = false;
+            LNameTxtbox.Visible = false;
+            LastNameLbl.Visible = false;
+            ConfirmPWLbl.Visible = false;
+            ConfirmPassTxtBox.Visible = false;
+            RegisterBtn.Visible = false;
+        }
+
+        protected void SignUpBtn_Click(object sender, EventArgs e)
+        {
+            EmailLbl.Visible = false;
+            EmailTxtBox.Visible = false;
+            PasswordLbl.Visible = false;
+            PasswordTxtBox.Visible = false;
+
+            NewEmailTxtBox.Visible = true;
+            SignupEmailLbl.Visible = true;
+            NewPassTxtBox.Visible = true;
+            NewPasswordLbl.Visible = true;
+            FNameLbl.Visible = true;
+            FNameTxtBox.Visible = true;
+            LNameTxtbox.Visible = true;
+            LastNameLbl.Visible = true;
+            ConfirmPWLbl.Visible = true;
+            ConfirmPassTxtBox.Visible = true;
+            RegisterBtn.Visible = true;
+        }
+
+        protected void LoginBtn_Click(object sender, EventArgs e)
+        {
+            string userName = EmailTxtBox.Text;
+            string password = PasswordTxtBox.Text;
+            Session["UserName"] = userName;
+            bool ifUserNameExists;
+            bool correctPassword;
+
+            SqlConnection userDb = new SqlConnection(SqlDataSource1.ConnectionString);
+            userDb.Open();
+            using (SqlCommand checkCmd = new SqlCommand("select count(*) from [user] where User_Username = @User_Username", userDb))
+            {
+                checkCmd.Parameters.AddWithValue("@User_Username", userName);
+                ifUserNameExists = (int)checkCmd.ExecuteScalar() > 0;
+            }
+
+        }
+
+        protected void RegisterBtn_Click(object sender, EventArgs e)
+        {
+            string userName = NewEmailTxtBox.Text;
+            string password = NewPassTxtBox.Text;
+            string firstName = FNameTxtBox.Text;
+            string lastName = LNameTxtbox.Text;
+            string confirmPW = ConfirmPassTxtBox.Text;
+            Session["UserName"] = userName;
+            bool ifUserNameExists;
+
+            SqlConnection userDb = new SqlConnection(SqlDataSource1.ConnectionString);
+            userDb.Open();
+            using (SqlCommand checkCmd = new SqlCommand("select count(*) from username where email = @email", userDb))
+            {
+                checkCmd.Parameters.AddWithValue("@email", userName);
+                ifUserNameExists = (int)checkCmd.ExecuteScalar() > 0;
+            }
+
+            if (ifUserNameExists || userName == "" || !IsValidEmail(userName))
+            {
+
+                error1.ForeColor = System.Drawing.Color.Red;
+                error1.Text = "Sorry, that user name is already taken, or you left the field blank.";
+                error1.Visible = true;
+                userDb.Close();
+                // checks email to see if valid. Function is at bottom of code
+                if (!IsValidEmail(userName))
+                {
+                    error2.ForeColor = System.Drawing.Color.Red;
+                    error2.Text = "Invalid email address.";
+                    error2.Visible = true;
+                }
+            }
+            else
+            {
+
+                // create sql command instance
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = System.Data.CommandType.Text;
+                // create sql command
+                cmd.CommandText = "insert into username (email) values (@email)";
+                // add values to sql table
+                cmd.Parameters.AddWithValue("@email", userName);
+                cmd.Connection = userDb;
+
+                // Problem here...Code is going to the catch statement here. 
+
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+
+                    error1.ForeColor = System.Drawing.Color.Black;
+                    error1.Text = "User name is accepted!";
+                    error1.Visible = true;
+
+                    // Response.Redirect("/page2.aspx");
+                }
+                catch
+                {
+
+                    error1.ForeColor = System.Drawing.Color.Red;
+                    error1.Text = "Error writing into the database";
+                    error1.Visible = true;
+
+                }
+                finally
+                {
+                    userDb.Close();
+                }
+
+            }
+
+
+        }
+
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
