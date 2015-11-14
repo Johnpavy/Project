af4a95094faf1a21289a8d0b60405af0cb0d9d03
Johnpavy <johnpavelchak@yahoo.com>
2015-11-14 14:34:42 -0600

    test

--- ProjectManagement/AdminPage.aspx.cs
+++ ProjectManagement/AdminPage.aspx.cs
@@ -7,16 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace ProjectManagement
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class AdminPage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        protected void AdminLoginButton_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("AdminPage.aspx");
         }
     }
 }
