0cfee6251125113167c28f931339e75028afd7cb
Johnpavy <johnpavelchak@yahoo.com>
2015-11-14 15:19:07 -0600

    added admin login password check and added default user id numbers to 1

--- ProjectManagement/adminLogin.aspx.cs
+++ ProjectManagement/adminLogin.aspx.cs
@@ -30,14 +30,48 @@ namespace ProjectManagement
             userDb.Open();
             //test
             // this function is to check and see if the username exists in database. If the scalar value returns >0, ifUserNameExists is set to true
-            using (SqlCommand checkCmd = new SqlCommand("select count(*) from username where email = @email", userDb))
+            using (SqlCommand checkCmd = new SqlCommand("select count(*) from username where email = @email and groupid = @groupid", userDb))
             {
                 checkCmd.Parameters.AddWithValue("@email", adminUser);
+                checkCmd.Parameters.AddWithValue("@groupid", "0");
                 ifUserNameExists = (int)checkCmd.ExecuteScalar() > 0;
             }
 
+            if (ifUserNameExists)
+            {
+                // check to see if username and pw combo exists
+                using (SqlCommand checkCmd = new SqlCommand("select count(*) from username where email = @email and password = @password" , userDb))
+                {
+                    checkCmd.Parameters.AddWithValue("@email", adminUser);
+                    checkCmd.Parameters.AddWithValue("@password", password);
 
-            Response.Redirect("AdminPage.aspx");
+                    correctPassword = (int)checkCmd.ExecuteScalar() > 0;
+                }
+                // if both match, on to page 1(or whatever we call it)
+                if (correctPassword)
+                {
+                    // I put admin login as a placeholder for now
+
+                    Response.Redirect("/AdminPage.aspx");
+                }
+                // we already confirmed that a username exists at this point so now we know that the password doesn't match
+                else
+                {
+                    loginLabel.ForeColor = System.Drawing.Color.Red;
+                    loginLabel.Text = "Incorrect password.";
+                    loginLabel.Visible = true;
+                }
+
+            }
+            // if the username doesnt exists, this code executes
+            else
+            {
+                loginLabel.ForeColor = System.Drawing.Color.Red;
+                loginLabel.Text = "User name not found.";
+                loginLabel.Visible = true;
+            }
+
+        
         }
     }
 }
--- ProjectManagement/adminLogin.aspx.designer.cs
+++ ProjectManagement/adminLogin.aspx.designer.cs
@@ -40,6 +40,15 @@ namespace ProjectManagement {
         protected global::System.Web.UI.WebControls.Button AdminLoginButton;
         
         /// <summary>
+        /// loginLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label loginLabel;
+        
+        /// <summary>
         /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
