43f3e297416ad76d4957f1cf44d4f251d0093379
li-nt <michael@mincey.org>
2015-11-24 18:23:51 -0600

    masterpage

--- ProjectManagement/Menu.Master.designer.cs
+++ ProjectManagement/Menu.Master.designer.cs
@@ -49,6 +49,15 @@ namespace ProjectManagement {
         protected global::System.Web.UI.WebControls.Label greeting;
         
         /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
         /// HyperLink1 control.
         /// </summary>
         /// <remarks>
