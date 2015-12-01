ae40d0ec734614243007da89793a024678c1579a
Luke Hitch <lhitch08@aol.com>
2015-11-30 20:26:25 -0600

    Added Search function

--- ProjectManagement/Menu.Master.cs
+++ ProjectManagement/Menu.Master.cs
@@ -29,5 +29,30 @@ namespace ProjectManagement
             //Present greeting on login page
             greeting.Text = "Welcome back, " + firstname;
         }
+
+        protected void searchBtn_Click(object sender, EventArgs e)
+        {
+            //do nothing if empty string
+            if (searchTxt.Text == "")
+                return;
+
+            SqlConnection db = new SqlConnection(SqlDataSource1.ConnectionString);
+            db.Open();
+            string search = searchTxt.Text;
+            SqlCommand searchproj = new SqlCommand();
+            searchproj.CommandText = "SELECT * FROM projects WHERE projectname LIKE '%'+@search+'%'";
+            searchproj.Parameters.AddWithValue("@search",search);
+            searchproj.Connection = db;
+            SqlDataReader searchread = searchproj.ExecuteReader();
+            if (searchread.Read())
+            {
+                string searchresult = searchread.GetString(0);
+                searchLbl.Text = searchresult;
+            }
+            else
+            {
+                searchLbl.Text = "No project with that term found.";
+            }
+        }
     }
 }
--- ProjectManagement/Menu.Master.designer.cs
+++ ProjectManagement/Menu.Master.designer.cs
@@ -58,6 +58,33 @@ namespace ProjectManagement {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
+        /// searchBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button searchBtn;
+        
+        /// <summary>
+        /// searchTxt control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox searchTxt;
+        
+        /// <summary>
+        /// searchLbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label searchLbl;
+        
+        /// <summary>
         /// HyperLink1 control.
         /// </summary>
         /// <remarks>
